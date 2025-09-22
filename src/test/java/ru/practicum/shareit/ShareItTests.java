4e59bd919e47df888c0f13fbfc515e23ee52411a
egopv <egop.valuev@yandex.ru>
2025-09-22 13:48:42 +0300

    Refacor1

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -3,7 +3,7 @@ package ru.practicum.shareit;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ru.yandex.practicum.ShareItApplication.class)
 class ShareItTests {
 
     @Test
